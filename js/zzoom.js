df8d20669e0fb50c4c74b3d0adb9bc10b1f2363d
Rita D'Aprano <rita.daprano@gmail.com>
2014-01-21 12:18:30 +0100

    Get mousemove event on image

--- js/zzoom.js
+++ js/zzoom.js
@@ -10,34 +10,31 @@
 
       var $circle =  $( "<div></div>");
 
-      var plus = 0;
+      var mouseX;
+      var mouseY;
 
       $image.on({
 
-        mouseenter: function(event) {
+        mouseenter: function() {
           $circle.insertBefore(this);
           console.log('hello!');
-          $circle.on({
-            mousemove: function(e) {
-              console.log('dd');
-              $(this).css({
-                top: (e.pageY - 50),
-                left: (e.pageX - 50)
-              });
-            }
-          });
-          $image.animate({
-            top: (event.pageY - 50),
-            left: (event.pageX - 50),
+          $(this).animate({
             width: $image.width() * 1.2,
             height: $image.height() * 1.2,
             display: 'block'
-          });
+          })
+        },
+        mousemove: function(e) {
+           console.log(mouseX , mouseY);
+          mouseX = e.clientX; 
+          mouseY = e.clientY;
+          
         },
         mouseout: function() {
-          $image.animate({
+          console.log('bye!');
+          $(this).animate({
             width: $image.width() / 1.2,
-            height: $image.height()  / 1.2,
+            height: $image.height()  / 1.2
           });
         }
       });
