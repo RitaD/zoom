a6358d8aedf2e930e52e26d1ea002efc7c999cc9
Rita D'Aprano <rita.daprano@gmail.com>
2014-01-21 19:25:02 +0100

    Clean code and Fixes bug on mousenter

--- js/zzoom.js
+++ js/zzoom.js
@@ -1,45 +1,36 @@
 (function( $ ) {
 
   $.fn.zzoom = function() {
-
-
-
     this.each( function() {
-
       var $image = $('.zzomIn').find('#zoom');
-
       var $circle =  $( "<div></div>");
-
       var mouseX;
       var mouseY;
-      
-
 
       $('.zzomIn').on({
         mouseenter: function() {
-          console.log('enter!');
-           $image.animate({
+           $image.css({
             width: "200%",
             height: "200%",
             display: 'block',
             'z-index': 0
-          })
+          });
         },
+
         mousemove: function(e) {
           var amountMovedX = (e.pageX * -1 / 2);
           var amountMovedY = (e.pageY * -1 / 2);
-          console.log('move!');
-          mouseX = e.clientX; 
+          mouseX = e.clientX;
           mouseY = e.clientY;
            $image.css ({
             'top' : amountMovedY,
             'left': amountMovedX,
             'position' : 'relative'
-          })
+          });
         },
+
         mouseleave: function() {
-          console.log('bye!');
-          $image.animate({
+          $image.css({
             width: "100%",
             height: "100%",
             top: 0,
@@ -47,10 +38,9 @@
             'z-index': 100
           });
         }
-      })
-
-
+      });
     });
-  }
+  };
+
   return this;
 }( jQuery ));
