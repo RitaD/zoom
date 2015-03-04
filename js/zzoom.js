2ab4ad0a6e57e5b1ed00f5455c8a3bef1c7a0351
Rita D'Aprano <rita.daprano@gmail.com>
2015-03-04 11:49:23 +0100

    Adds a refactor

--- js/zzoom.js
+++ js/zzoom.js
@@ -3,19 +3,19 @@
   $.fn.zzoom = function() {
     this.each( function() {
       var $container = $('.zzomIn');
-      var boxWidth = $(".zoom").width();
-      var boxHeight = $(".zoom").height();
       var $image = $container.find('.zoom');
+      var $imageW = $image.width();
+      var $imageH = $image.height();
+      var $containerW = $container.width($imageW);
+      var $containerH = $container.height($imageH);
       var $imageClone = $image.clone();
-      var $imageLarge = $imageClone.width(boxWidth * 2);
-      var imageH = $image.height();
-      var imageW = $image.width();
+      var $imageLarge = $imageClone.width($imageW * 2);
       var imageLargeW = $imageLarge.width();
       var imageLargeH = $imageLarge.height();
 
-      $('.zzomIn')
-        .width(boxWidth)
-        .height(boxHeight);
+      $image
+        .width($imageW)
+        .height($imageH);
         
       $('.zzomIn').on({
 
@@ -26,8 +26,8 @@
         mousemove: function(e) {
           var mouseX = e.pageX - $(this).offset().left;
           var mouseY = e.pageY - $(this).offset().top;
-          var amountMovedX = (Math.round((mouseX/imageW)*100)/100) * (imageLargeW - imageW);
-          var amountMovedY = (Math.round((mouseY/imageH)*100)/100) * (imageLargeH - imageH);
+          var amountMovedX = (Math.round((mouseX/$imageW)*100)/100) * (imageLargeW - $imageW);
+          var amountMovedY = (Math.round((mouseY/$imageH)*100)/100) * (imageLargeH - $imageH);
            $imageLarge.css ({
             'top' : amountMovedY + 'px',
             'left': -amountMovedX + 'px',
